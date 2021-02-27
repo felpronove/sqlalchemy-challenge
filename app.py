bf1fd82f5e7f19ed27382e09982f764b9c2d228c
Felix Pronove <felixpronove4@gmail.com>
2021-02-26 18:38:00 -0700

    Histogram fixed

--- app.py
+++ app.py
@@ -78,3 +78,8 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 def start():
     """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range."""
+    """When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date."""
+    session=Session(engine)
+
+    results=session.query(Measurements.station, func.min(Measurements.tobs), func.max(Measurements.tobs), func.avg(Measurements.tobs)).\
+    group_by(Measurements.station).all()
